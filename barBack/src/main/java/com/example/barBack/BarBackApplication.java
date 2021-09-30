a80f28cc62ba9108c2080cb4dcc8fac3de431ea9
KCherd <krisacherd@gmail.com>
2021-09-30 16:13:27 +0700

    init commit

---
+++ barBack/src/main/java/com/example/barBack/BarBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.barBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarBackApplication.class, args);
+	}
+
+}
---
+++ barBack/src/test/java/com/example/barBack/BarBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.barBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
