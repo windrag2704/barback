fbcd8f8f70fe1c1c31f80ef5c43f76a9c494353f
Евгений Морозов <windrag000@gmail.com>
2021-12-07 13:20:34 +0300

    fix queries

--- barBack/src/main/java/com/example/barBack/repository/CustomerRepository.java
+++ barBack/src/main/java/com/example/barBack/repository/CustomerRepository.java
@@ -4,14 +4,15 @@ import java.util.Set;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.example.barBack.model.Customer;
 import com.example.barBack.model.Good;
 
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
     @Query("select c from Customer c where c.phone = :phone")
-    Customer findCustomerByPhone(String phone);
-    @Query("select fav.favourites from Customer fav")
-    Set<Good> getCustomerFavourites(Long id);
+    Customer findCustomerByPhone(@Param("phone") String phone);
+    @Query("select cus.favourites from Customer cus where cus.id = :id")
+    Set<Good> getCustomerFavourites(@Param("id") Long id);
     Customer getCustomerById(Long id);
 }
